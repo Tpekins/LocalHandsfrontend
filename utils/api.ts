0c1b36b255205d4a94abd819d46a9ed642b382c6
Tpekins <tianipekins5@gmail.com>
2026-04-11 14:46:39 +0100

    Fix vercel.json syntax

--- utils/api.ts
+++ utils/api.ts
@@ -11,7 +11,7 @@ import { toast } from "sonner";
 
 const baseURL: string =
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  (import.meta.env.VITE_API_BASE_URL as string) || "http://localhost:3000/api";
+  (import.meta.env.VITE_API_URL as string) || "http://localhost:3000/api";
 
 const api = axios.create({
   baseURL,
